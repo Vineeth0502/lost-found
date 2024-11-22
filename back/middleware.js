49a3e9c022a79a59feb4a08e790440fc1fb820f9
vineeth ketham <vineethketham@gmail.com>
2024-11-22 15:50:43 -0600

    changes updated sucessfully

--- back/middleware.js
+++ back/middleware.js
@@ -8,7 +8,7 @@ exports.requireSignin=(req,res,next)=>{
         // console.log("Request is : ",req.body)
         const token=req.headers.authorization.split(" ")[1]
         // console.log(token)
-        const user =jwt.verify(token,process.env.JWT_SECRET)
+        const user =jwt.verify(token,"marvel")
         // console.log("User is : ",user)
         req.user=user
         req.role='user'
